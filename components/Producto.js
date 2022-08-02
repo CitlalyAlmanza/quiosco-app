43a01c2b7eca087cc2f5fdc7de95e87c9dc00016
Citlaly <sandicitlaly18@gmail.com>
2022-08-01 20:09:01 -0500

    App con next.js

---
+++ components/Producto.js
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import { formatearDinero } from '../helpers'
+import useQuiosco from '../hooks/useQuiosco'
+
+const Producto = ({producto}) => {
+  const {handleCSetProducto, handleChangeModal} = useQuiosco()
+    const {nombre, imagen, precio} = producto
+  return (
+    <div className="border p-3">
+        <Image width={400} height={500} src={`/assets/img/${imagen}.jpg`} alt={`Imagen ${nombre}`}
+        />
+        <div className='p-5'>
+            <h3 className='text-2xl font-bold'>{nombre}</h3>
+            <p className='mt-5 font-black text-4xl text-amber-500'>
+                {formatearDinero(precio)}
+            </p>
+        </div>
+        <button
+          type='button'
+          className='bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold'
+          onClick={()=>{
+            handleChangeModal()
+            handleCSetProducto(producto)
+          }}
+        >
+          Agregar
+        </button>
+    </div>
+  )
+}
+
+export default Producto
